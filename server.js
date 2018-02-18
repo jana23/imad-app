146a23507f725f1c6b2dbfd1c8dad6cc9cbe0a29
jana23 <janarthanan.siva123@gmail.com>
2018-02-18 13:18:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/art-one.html',function(req,res){
-    
+app.get('/art-one.html',function (req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'art-one.html'));
 });
 
